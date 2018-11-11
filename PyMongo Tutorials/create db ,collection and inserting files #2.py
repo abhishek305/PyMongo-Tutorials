cf5a7bd631630fc0fe6673611bce4aa7890c08e8
Abhishek Ezhava <Abhishekshaji32@gmail.com>
2018-11-11 21:07:25 +0530

    Update create db ,collection and inserting files #2.py

--- PyMongo Tutorials/create db ,collection and inserting files #2.py
+++ PyMongo Tutorials/create db ,collection and inserting files #2.py
@@ -5,20 +5,20 @@ mydb = myclient["demo"]
 mycoll=mydb["dbtable"]
 
 mylist = [
-  { "_id": 1, "name": "John", "address": "Highway 37"},
-  { "_id": 2, "name": "Peter", "address": "Lowstreet 27"},
-  { "_id": 3, "name": "Amy", "address": "Apple st 652"},
-  { "_id": 4, "name": "Hannah", "address": "Mountain 21"},
-  { "_id": 5, "name": "Michael", "address": "Valley 345"},
-  { "_id": 6, "name": "Sandy", "address": "Ocean blvd 2"},
-  { "_id": 7, "name": "Betty", "address": "Green Grass 1"},
-  { "_id": 8, "name": "Richard", "address": "Sky st 331"},
-  { "_id": 9, "name": "Susan", "address": "One way 98"},
-  { "_id": 10, "name": "Vicky", "address": "Yellow Garden 2"},
-  { "_id": 11, "name": "Ben", "address": "Park Lane 38"},
-  { "_id": 12, "name": "William", "address": "Central st 954"},
-  { "_id": 13, "name": "Chuck", "address": "Main Road 989"},
-  { "_id": 14, "name": "Viola", "address": "Sideway 1633"}
+  { "id": 1, "name": "John", "address": "Highway 37"},
+  { "id": 2, "name": "Peter", "address": "Lowstreet 27"},
+  { "id": 3, "name": "Amy", "address": "Apple st 652"},
+  { "id": 4, "name": "Hannah", "address": "Mountain 21"},
+  { "id": 5, "name": "Michael", "address": "Valley 345"},
+  { "id": 6, "name": "Sandy", "address": "Ocean blvd 2"},
+  { "id": 7, "name": "Betty", "address": "Green Grass 1"},
+  { "id": 8, "name": "Richard", "address": "Sky st 331"},
+  { "id": 9, "name": "Susan", "address": "One way 98"},
+  { "id": 10, "name": "Vicky", "address": "Yellow Garden 2"},
+  { "id": 11, "name": "Ben", "address": "Park Lane 38"},
+  { "id": 12, "name": "William", "address": "Central st 954"},
+  { "id": 13, "name": "Chuck", "address": "Main Road 989"},
+  { "id": 14, "name": "Viola", "address": "Sideway 1633"}
 ]
 
 x = mycoll.insert_many(mylist)
